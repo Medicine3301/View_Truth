a46b761f22d8a51cb5994d9d0f2886040a01201d
Medicine3301 <acr3214@gmail.com>
2025-04-08 15:51:21 +0800

    fix:vuetigy css problem

--- view_the_truth/src/main.js
+++ view_the_truth/src/main.js
@@ -5,11 +5,7 @@ import router from './router'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/reset.css';
 import * as AntDesignIconsVue from '@ant-design/icons-vue'
-// Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+
 const app = createApp(App)
 const pinia = createPinia()
 
@@ -17,14 +13,11 @@ const pinia = createPinia()
 for (const [key, component] of Object.entries(AntDesignIconsVue)) {
     app.component(key, component)
 }
-const vuetify = createVuetify({
-    components,
-    directives,
-  })
+
 app.use(pinia)
     .use(router)
     .use(Antd)  // 使用 Ant Design Vue
-    .use(vuetify)// 使用vuetif
+    
     .mount('#app')
 
 // 主入口文件：创建 Vue 应用实例，集成 Pinia、Vue Router、Element Plus 和 Ant Design Vue
